5c47c50c78da1f3fce80e5fefb6ee6792bc87a40
烽正 楊 <iefcyang@gmail.com>
2019-09-11 08:15:40 +0800

    加入專案檔案。

---
+++ ImageAISolver/MainForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ImageAISolver
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pcbImage = new System.Windows.Forms.PictureBox();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnGray = new System.Windows.Forms.Button();
+            this.btnBinary = new System.Windows.Forms.Button();
+            this.btnColLine = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.labMessage = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImage)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pcbImage);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnColLine);
+            this.splitContainer1.Panel2.Controls.Add(this.btnBinary);
+            this.splitContainer1.Panel2.Controls.Add(this.btnGray);
+            this.splitContainer1.Panel2.Controls.Add(this.btnOpen);
+            this.splitContainer1.Size = new System.Drawing.Size(1186, 692);
+            this.splitContainer1.SplitterDistance = 994;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pcbImage
+            // 
+            this.pcbImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pcbImage.Location = new System.Drawing.Point(0, 0);
+            this.pcbImage.Name = "pcbImage";
+            this.pcbImage.Size = new System.Drawing.Size(994, 692);
+            this.pcbImage.TabIndex = 0;
+            this.pcbImage.TabStop = false;
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.DefaultExt = "jpg";
+            this.dlgOpen.FileName = "openFileDialog1";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(125, 35);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // btnGray
+            // 
+            this.btnGray.Location = new System.Drawing.Point(12, 53);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(125, 35);
+            this.btnGray.TabIndex = 1;
+            this.btnGray.Text = "Gray Image";
+            this.btnGray.UseVisualStyleBackColor = true;
+            this.btnGray.Click += new System.EventHandler(this.BtnGray_Click);
+            // 
+            // btnBinary
+            // 
+            this.btnBinary.Location = new System.Drawing.Point(12, 94);
+            this.btnBinary.Name = "btnBinary";
+            this.btnBinary.Size = new System.Drawing.Size(125, 35);
+            this.btnBinary.TabIndex = 2;
+            this.btnBinary.Text = "Binary B/W";
+            this.btnBinary.UseVisualStyleBackColor = true;
+            // 
+            // btnColLine
+            // 
+            this.btnColLine.Location = new System.Drawing.Point(12, 147);
+            this.btnColLine.Name = "btnColLine";
+            this.btnColLine.Size = new System.Drawing.Size(125, 49);
+            this.btnColLine.TabIndex = 3;
+            this.btnColLine.Text = "Column Separators";
+            this.btnColLine.UseVisualStyleBackColor = true;
+            this.btnColLine.Click += new System.EventHandler(this.BtnColLine_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.labMessage});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 692);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1186, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // labMessage
+            // 
+            this.labMessage.Name = "labMessage";
+            this.labMessage.Size = new System.Drawing.Size(1171, 17);
+            this.labMessage.Spring = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1186, 714);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Character Recognizer";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImage)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pcbImage;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.Button btnBinary;
+        private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.Button btnColLine;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel labMessage;
+    }
+}
+
---
+++ ImageAISolver/MainForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageAISolver
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            if( dlgOpen.ShowDialog() == DialogResult.OK     )
+            {
+                origin = new Bitmap(dlgOpen.FileName);
+                pcbImage.Image = origin;
+                pcbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+        Bitmap origin;
+        Bitmap grayImage;
+        private void BtnGray_Click(object sender, EventArgs e)
+        {
+ 
+
+            grayImage = new Bitmap(origin.Width,origin.Height);
+           // Graphics grayG = Graphics.FromImage(grayImage);
+            
+            for ( int r = 0; r < grayImage.Height; r++ )
+                for( int c = 0; c < grayImage.Width; c++ )
+                {
+                    Color clr = origin.GetPixel(c, r);
+                    int v = (clr.R + clr.G + clr.B) / 3;
+                    grayImage.SetPixel(c, r, Color.FromArgb( v,v,v ));
+                }
+            pcbImage.Image = grayImage;
+        }
+
+        int hOff, hSize, hcount, offLimit;
+        byte tol = 10;
+
+        private void BtnColLine_Click(object sender, EventArgs e)
+        {
+            int trials = 20;
+            int vSize = origin.Height / 20;
+            int vStart = 3 * vSize / 2;
+            byte vv;
+            bool OK = false;
+            for(  hcount = 5; hcount < 20; hcount++)
+            {
+                byte v;
+                hSize = origin.Width / hcount;
+                int start = hSize * 3 / 2;
+                int end = start + hSize;
+                int x =  start;
+                int y = vStart;
+               for( hOff = start; hOff < end; hOff ++ )
+                {
+                    OK = true;
+                    v = grayImage.GetPixel(x, y).R;
+                    for( int c = 0; c < hcount-1; c++ )
+                    {
+                         x = c * hSize + hOff;
+                        for( int r = 0; r < 19; r ++ )
+                        {
+                             y = vStart + vSize * r;
+                            vv = grayImage.GetPixel(x, y).R;
+                            if( vv - v > tol && v - vv > tol )
+                            {
+                                OK = false;
+                                break;
+                            }
+                        }
+                        if (!OK) break;
+                    }
+                    if (OK)
+                    {
+                        // 成功
+                        break;
+                    }
+                }
+                if (OK) break;
+            }
+
+            labMessage.Text = $"hoff = {hOff} hsize = {hSize} hcount = {hcount}";
+
+        }
+    }
+}
